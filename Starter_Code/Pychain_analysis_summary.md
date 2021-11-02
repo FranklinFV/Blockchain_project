b6d3b80a5a707a89732b7ee9fa2a9a902857bdb1
FranklinFV <84001493+FranklinFV@users.noreply.github.com>
2021-11-01 23:28:28 -0500

    Adding the final code and images for the analysis.

---
+++ Starter_Code/Pychain_analysis_summary.md
@@ -0,0 +1,26 @@
+# Blockchain - PyChain Ledger
+## By Franklin Vaca
+<p>The program/code can be found in the pychian.py file which written using Visual Studio. 
+The application was created using streamlit and it can be run using Git Bash or Terminal (Git Bash was used for this project). The pictures presented below are located in the Analysis_images folder within the Starter_code. The screenshots display the application at work, storing new transactions on each new block added to the chain.</p>
+<p></p>
+
+## **Application screenshot:**
+
+*PyChain Application* 
+
+![image](Analysis_images/Application_screenshot.PNG)
+<p></p><br>
+
+
+
+<p>The PyChain ledger displays the initial block "0" and also three additional blocks that were created by the user identified as "Creatir_id: 42". The timestamp and nonce are also included as part of the blocks.</p>
+<p>The Validate Chain button indicates a "True" value which validates the different blocks included in the ledger.</p>
+
+## **Git Bash PyChain Application Information:**
+
+*Git Bash information* 
+![image](Analysis_images/Git_bash_validation.PNG)
+<p></p><br>
+
+
+<p> The chain was initialized and the local and network URLs are displayed for the user to view the Ledger. Also, the wining hash information is available for traceability. No error messages were found.</p>
--- Starter_Code/pychain.py
+++ Starter_Code/pychain.py
@@ -50,6 +50,11 @@ import hashlib
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
 # YOUR CODE HERE
+@dataclass
+class Record:
+    sender: str
+    receiver: str
+    amount: float
 
 
 ################################################################################
@@ -68,8 +73,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
-
+    record: Record
     creator_id: int
     prev_hash: str = 0
     timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
@@ -166,19 +170,22 @@ pychain = setup()
 
 # @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
+# input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
 # YOUR CODE HERE
+sender = st.text_input("sender")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
 # YOUR CODE HERE
+receiver = st.text_input("receiver")
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
 # YOUR CODE HERE
+amount = st.text_input("amount")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -189,7 +196,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        record=(sender, "sent", amount, "to", receiver),
         creator_id=42,
         prev_hash=prev_block_hash
     )
@@ -202,7 +209,7 @@ if st.button("Add Block"):
 
 st.markdown("## The PyChain Ledger")
 
-pychain_df = pd.DataFrame(pychain.chain)
+pychain_df = pd.DataFrame(pychain.chain).astype("str")
 st.write(pychain_df)
 
 difficulty = st.sidebar.slider("Block Difficulty", 1, 5, 2)
